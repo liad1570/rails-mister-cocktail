b4c79babbc83c374c609806823115790d4459306
liad peisic <liad1570@walla.com>
2019-02-14 16:58:55 +0200

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'simple_form'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.9.1'
 
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,10 +11,25 @@
  puts "I am destroying all cocktails"
 Dose.destroy_all
 Cocktail.destroy_all
-Ingredient.destroy_all
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+ Ingredient.destroy_all
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
 
 
+
+
+
+# Go fetch the contents of a URL & store them as a String
+response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+
+# "Pretty prints" the result to look like a web page instead of one long string of HTML
+ data = JSON.parse(response)
+ data =  data["drinks"]
+  data.each do |d|
+  	 d["strIngredient1"]
+  	Ingredient.create(name:d["strIngredient1"] )
+  	 
+ 
+  end
 
